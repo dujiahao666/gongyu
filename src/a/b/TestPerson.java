b9e2d7d3949ed5ea89b64ad01955d036a06dc2f5
dujiahao666 <188216625335@163.com>
2024-10-29 12:49:33 +0800

    test commit

--- src/a/b/TestPerson.java
+++ src/a/b/TestPerson.java
@@ -9,7 +9,10 @@ package a.b;
 public class TestPerson {
     public static void main(String[] args) {
         Person person1 = new Person("君君","咸阳",29);
+        System.out.println();
         person1=new Person();
         System.out.println(person1);
+        System.out.println(1);
+        System.out.println(1);
     }
 }
